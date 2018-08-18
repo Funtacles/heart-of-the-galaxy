8518d80d0b524dae95c4fda63d340cf02f41d2b1
Funtacles <funtacles@gmail.com>
2018-08-18 14:00:25 -0600

    Fix gitignore

---
+++ src/com/lilithsthrone/data/ImportExportManager.java
@@ -0,0 +1,125 @@
+package com.lilithsthrone.data;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import com.lilithsthrone.game.PropertyValue;
+import com.lilithsthrone.game.character.CharacterImportSetting;
+import com.lilithsthrone.game.character.CharacterUtils;
+import com.lilithsthrone.game.dialogue.responses.Response;
+import com.lilithsthrone.game.dialogue.story.CharacterCreation;
+import com.lilithsthrone.main.Main;
+import com.lilithsthrone.utils.Colour;
+
+public class ImportExportManager {
+
+	public static List<File> getCharactersForImport() {
+		List<File> filesList = new ArrayList<>();
+		
+		File dir = new File("data/characters");
+		if (dir.isDirectory()) {
+			File[] directoryListing = dir.listFiles((path, name) -> name.endsWith(".xml"));
+			if (directoryListing != null) {
+				filesList.addAll(Arrays.asList(directoryListing));
+			}
+		}
+
+		filesList.sort(Comparator.comparingLong(File::lastModified).reversed());
+		
+		return filesList;
+	}
+
+	public static List<File> getSlavesForImport() {
+		List<File> filesList = new ArrayList<>();
+		
+		File dir = new File("data/characters");
+		if (dir.isDirectory()) {
+			File[] directoryListing = dir.listFiles((path, name) -> name.endsWith(".xml"));
+			if (directoryListing != null) {
+				filesList.addAll(Arrays.asList(directoryListing));
+			}
+		}
+		
+		filesList.sort(Comparator.comparingLong(File::lastModified).reversed());
+		
+		return filesList;
+	}
+
+	public static void deleteExportedCharacter(String name) {
+		File file = new File("data/characters/"+name+".xml");
+
+		if (file.exists()) {
+			try {
+				file.delete();
+				Main.game.setContent(new Response("", "", Main.game.getCurrentDialogueNode()));
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			
+		} else {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "File not found...");
+		}
+	}
+
+	public static List<File> getGamesForImport() {
+		List<File> filesList = new ArrayList<>();
+		
+		File dir = new File("data/saves");
+		if (dir.isDirectory()) {
+			File[] directoryListing = dir.listFiles((path, name) -> name.endsWith(".xml"));
+			if (directoryListing != null) {
+				filesList.addAll(Arrays.asList(directoryListing));
+			}
+		}
+
+		filesList.sort(Comparator.comparingLong(File::lastModified).reversed());
+		
+		return filesList;
+	}
+
+	public static void deleteExportedGame(String name) {
+		File file = new File("data/saves/"+name+".xml");
+
+		if (file.exists()) {
+			try {
+				file.delete();
+				Main.game.setContent(new Response("", "", Main.game.getCurrentDialogueNode()));
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			
+		} else {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "File not found...");
+		}
+	}
+
+	public static void importCharacter(File file) {
+		if (file != null) {
+			try {
+				Main.game.setPlayer(CharacterUtils.startLoadingCharacterFromXML());
+				Main.game.setPlayer(CharacterUtils.loadCharacterFromXML(file, Main.game.getPlayer(),
+						CharacterImportSetting.NO_PREGNANCY,
+						CharacterImportSetting.NO_COMPANIONS,
+						CharacterImportSetting.CLEAR_SLAVERY));
+				
+				Main.game.getPlayer().endPregnancy(false);
+				
+				Main.game.setRenderAttributesSection(true);
+				Main.game.clearTextStartStringBuilder();
+				Main.game.clearTextEndStringBuilder();
+				Main.getProperties().setValue(PropertyValue.newWeaponDiscovered, false);
+				Main.getProperties().setValue(PropertyValue.newClothingDiscovered, false);
+				Main.getProperties().setValue(PropertyValue.newItemDiscovered, false);
+				Main.game.getPlayer().calculateStatusEffects(0);
+
+				Main.game.initNewGame(CharacterCreation.START_GAME_WITH_IMPORT);
+				
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/com/lilithsthrone/data/SaveManager.java
@@ -0,0 +1,139 @@
+package com.lilithsthrone.data;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import com.lilithsthrone.game.Game;
+import com.lilithsthrone.game.character.body.valueEnums.Femininity;
+import com.lilithsthrone.game.character.quests.QuestLine;
+import com.lilithsthrone.game.dialogue.DialogueNodeType;
+import com.lilithsthrone.game.dialogue.responses.Response;
+import com.lilithsthrone.game.inventory.enchanting.TFEssence;
+import com.lilithsthrone.main.Main;
+import com.lilithsthrone.utils.Colour;
+
+public class SaveManager {
+
+    public static void createSaveFolders(){
+        File dir = new File("data/");
+		dir.mkdir();
+		dir = new File("data/saves");
+		dir.mkdir();
+		dir = new File("data/characters");
+		dir.mkdir();
+    }
+
+    public static void autoSave(){
+        SaveManager.saveGame("AutoSave_"+Main.game.getPlayer().getName(), true);
+    }
+
+    public static void quickSaveGame() {
+		if (Main.game.isInCombat()) {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "Cannot quicksave while in combat!");
+			
+		} else if (Main.game.isInSex()) {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "Cannot quicksave while in sex!");
+			
+		} else if (Main.game.getCurrentDialogueNode().getDialogueNodeType()!=DialogueNodeType.NORMAL) {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "Can only quicksave in a normal scene!");
+			
+		} else if (!Main.game.isStarted() || !Main.game.getCurrentDialogueNode().equals(Main.game.getDefaultDialogueNoEncounter())) {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "Cannot save in this scene!");
+			
+		} else {
+			Main.getProperties().lastQuickSaveName = "QuickSave_"+Main.game.getPlayer().getName();
+			SaveManager.saveGame("QuickSave_"+Main.game.getPlayer().getName(), true);
+		}
+	}
+
+	public static void quickLoadGame() {
+		String name = "QuickSave_"+Main.game.getPlayer().getName();
+		
+        SaveManager.loadGame(name);
+	}
+
+    public static List<File> getSavedGames() {
+		List<File> filesList = new ArrayList<>();
+		
+		File dir = new File("data/saves");
+		if (dir.isDirectory()) {
+			File[] directoryListing = dir.listFiles((path, name) -> name.endsWith(".xml"));
+			if (directoryListing != null) {
+				filesList.addAll(Arrays.asList(directoryListing));
+			}
+		}
+
+		filesList.sort(Comparator.comparingLong(File::lastModified).reversed());
+		
+		return filesList;
+    }
+
+    public static boolean isSaveGameAvailable() {
+		return Main.game.isStarted() && Main.game.getSavedDialogueNode() == Main.game.getDefaultDialogueNoEncounter();
+	}
+	
+	public static void saveGame(String name, boolean allowOverwrite) {
+		if (name.length()==0) {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "Name too short!");
+			return;
+		}
+		if (name.length() > 32) {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "Name too long!");
+			return;
+		}
+		if (!name.matches("[a-zA-Z0-9]+[a-zA-Z0-9' _]*")) {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "Incompatible characters!");
+			return;
+		}
+		
+		Game.exportGame(name, allowOverwrite);
+
+		try {
+			Main.getProperties().lastSaveLocation = name;//"data/saves/"+name+".lts";
+			Main.getProperties().nameColour = Femininity.valueOf(Main.game.getPlayer().getFemininityValue()).getColour().toWebHexString();
+			Main.getProperties().name = Main.game.getPlayer().getName();
+			Main.getProperties().level = Main.game.getPlayer().getLevel();
+			Main.getProperties().money = Main.game.getPlayer().getMoney();
+			Main.getProperties().arcaneEssences = Main.game.getPlayer().getEssenceCount(TFEssence.ARCANE);
+			Main.getProperties().race = Main.game.getPlayer().getSubspecies().getName();
+			Main.getProperties().quest = Main.game.getPlayer().getQuest(QuestLine.MAIN).getName();
+
+			Main.getProperties().savePropertiesAsXML();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static boolean isLoadGameAvailable(String name) {
+		File file = new File("data/saves/"+name+".xml");
+
+		return file.exists();
+	}
+	
+	public static void loadGame(String name) {
+		if (isLoadGameAvailable(name)) {
+			Game.importGame(name);
+		}
+	}
+	
+	public static void deleteGame(String name) {
+		File file = new File("data/saves/"+name+".xml");
+
+		if (file.exists()) {
+			try {
+				file.delete();
+				Main.game.setContent(new Response("", "", Main.game.getCurrentDialogueNode()));
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			
+		} else {
+			Main.game.flashMessage(Colour.GENERIC_BAD, "File not found...");
+		}
+	}
+    
+}
